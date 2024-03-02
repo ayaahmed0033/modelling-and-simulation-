a66f57559db556bd61f232a42e03609827fea813
Aya Ahmed <130912929+ayaahmed0033@users.noreply.github.com>
2024-03-02 20:55:48 +0200

    Add files via upload

---
+++ Template_Students/MultiQueueSimulation/MultiQueueModels/SimulationSystem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MultiQueueModels
+{
+    public class SimulationSystem
+    {
+        public SimulationSystem(int testcase)
+        {
+            this.Servers = new List<Server>();
+            this.InterarrivalDistribution = new List<TimeDistribution>();
+            this.PerformanceMeasures = new PerformanceMeasures();
+            this.SimulationTable = new List<SimulationCase>();
+            this.Testcase = testcase;
+
+
+        }
+        public int Testcase { get; set; }
+        public void test_num()
+        {
+            string path = @"C:\Users\Aya\Downloads\modeling and simulation\task 1\Template_Students\MultiQueueSimulation\MultiQueueSimulation\TestCases\TestCase" + Testcase.ToString() + ".txt";
+
+            string[] lines = File.ReadAllLines(path);
+
+            int lineIndex = 1;
+
+            // Read the NumberOfServers
+            NumberOfServers = int.Parse(lines[lineIndex]);
+
+            lineIndex += 3;
+            // Read the StoppingNumber
+            StoppingNumber = int.Parse(lines[lineIndex]);
+
+            lineIndex += 3;
+            // Read the StoppingCriteria
+            StoppingCriteria = (Enums.StoppingCriteria)int.Parse(lines[lineIndex]);
+
+            lineIndex += 3;
+            // Read the SelectionMethod
+            SelectionMethod = (Enums.SelectionMethod)int.Parse(lines[lineIndex]);
+
+            lineIndex += 3;
+            // Read the InterarrivalDistribution
+            InterarrivalDistribution = new List<TimeDistribution>();
+            while (lines[lineIndex] != "")
+            {
+                string[] distributionParts = lines[lineIndex].Split(',');
+                InterarrivalDistribution.Add(new TimeDistribution
+                {
+                    Time = int.Parse(distributionParts[0]),
+                    Probability = (decimal)double.Parse(distributionParts[1])
+                });
+                lineIndex++;
+            }
+
+            lineIndex += 2;
+            // Read the ServiceDistributions
+            Servers = new List<Server>();
+            for (int i = 0; i < NumberOfServers; i++)
+            {
+                List<TimeDistribution> serviceDistribution = new List<TimeDistribution>();
+                lineIndex++;
+                try
+                {
+                    while (lines[lineIndex] != "")
+                    {
+                        string[] distributionParts = lines[lineIndex].Split(',');
+                        serviceDistribution.Add(new TimeDistribution
+                        {
+                            Time = int.Parse(distributionParts[0]),
+                            Probability = (decimal)double.Parse(distributionParts[1])
+                        });
+                        lineIndex++;
+                    }
+                    
+                }
+                catch (Exception E) { };
+                Servers.Add(new Server { TimeDistribution = serviceDistribution });
+                lineIndex++;
+            }
+
+        }
+        public void find_interarrival()
+        {
+
+        }
+        public void all()
+        {
+            find_interarrival();
+             
+
+        }
+
+        ///////////// INPUTS ///////////// 
+        public int NumberOfServers { get; set; }
+        public int StoppingNumber { get; set; }
+        public List<Server> Servers { get; set; }
+        public List<TimeDistribution> InterarrivalDistribution { get; set; }
+        public Enums.StoppingCriteria StoppingCriteria { get; set; }
+        public Enums.SelectionMethod SelectionMethod { get; set; }
+
+        ///////////// OUTPUTS /////////////
+        public List<SimulationCase> SimulationTable { get; set; }
+        public PerformanceMeasures PerformanceMeasures { get; set; }
+
+    }
+}
