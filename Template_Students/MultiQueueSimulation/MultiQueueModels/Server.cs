93fc8bed1512a3c559836f6395c3e202af9d8add
usif-eid <yazqalany2004@gmail.com>
2024-03-06 14:02:22 +0200

    usif

--- Template_Students/MultiQueueSimulation/MultiQueueModels/Server.cs
+++ Template_Students/MultiQueueSimulation/MultiQueueModels/Server.cs
@@ -17,7 +17,6 @@ namespace MultiQueueModels
         public decimal IdleProbability { get; set; }
         public decimal AverageServiceTime { get; set; } 
         public decimal Utilization { get; set; }
-        public int min { get; set; }
         
         public bool avaialble { get; set; }
 
