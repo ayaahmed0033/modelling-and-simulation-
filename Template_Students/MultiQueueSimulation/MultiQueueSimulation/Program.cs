adc9fa90fa5f7aaa0493e0aec0dbb74fe3cf5574
Aya Ahmed <ayaahmed0033@gmail.com>
2024-03-20 00:05:25 +0200

    change choose

--- Template_Students/MultiQueueSimulation/MultiQueueSimulation/Program.cs
+++ Template_Students/MultiQueueSimulation/MultiQueueSimulation/Program.cs
@@ -30,26 +30,46 @@ namespace MultiQueueSimulation
             x[x.Length - 2] = "TestCases";
             string[] arr = { "TestCase1.txt", "TestCase2.txt", "TestCase3.txt"};   
             string result = "";
-            for(int i = 0; i < arr.Length; i++)
+           
+             
+            int choose =1;                                         //// change  choose numberrr ////
+            SimulationSystem system = new SimulationSystem();
+
+            String path = "";
+            // MessageBox.Show(path);
+
+            if (choose==1)
             {
-                SimulationSystem system = new SimulationSystem();
-                x[x.Length-1] = arr[i];
-                String path = string.Join("\\", x);
-                MessageBox.Show(path);
+                x[x.Length - 1] = arr[0];
+                path = string.Join("\\", x);
                 system.test_num(path);
                 system.all();
-                if (i == 0)
-                     result = TestingManager.Test(system, Constants.FileNames.TestCase1);
-                else if(i==1)
-                    result = TestingManager.Test(system, Constants.FileNames.TestCase2);
-                 else if(i==2)
-                    result = TestingManager.Test(system, Constants.FileNames.TestCase3);
+                result = TestingManager.Test(system, Constants.FileNames.TestCase1);
                 MessageBox.Show(result);
-
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1(system ,path));     // 7oto second paramter 3and forms 3ashan ta5do el path
             }
+            else if (choose.Equals(2))
+            {
+                x[x.Length - 1] = arr[1];
+                path = string.Join("\\", x);
+                system.test_num(path);
+                system.all();
+                result = TestingManager.Test(system, Constants.FileNames.TestCase2);
+                MessageBox.Show(result);
+            }
+            else if (choose.Equals(3))
+            {
+                x[x.Length - 1] = arr[2];
+                path = string.Join("\\", x);
+                system.test_num(path);
+                system.all();
+                result = TestingManager.Test(system, Constants.FileNames.TestCase3);
+                MessageBox.Show(result);
+            }
+
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(true);
+            //Application.Run(new Form1(system, path));     // 7oto second paramter 3and forms 3ashan ta5do el path
+
 
 
 
